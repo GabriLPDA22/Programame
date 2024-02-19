fb851f267a7ea7c31a945ae443d7c4941555a08e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-19 19:47:40 +0100

    Getters y setter añadidos

--- Challenge_#2_SALA_DE_CINE/espectador.java
+++ Challenge_#2_SALA_DE_CINE/espectador.java
@@ -8,4 +8,34 @@ class Espectador {
         this.edad = edad;
         this.dinero = dinero;
     }
+
+    public double getDinero()
+    {
+        return dinero;
+    }
+
+    public void setDinero(double dinero)
+    {
+        this.dinero = dinero;
+    }
+
+    public int getEdad()
+    {
+        return edad;
+    }
+
+    public void setEdad(int edad)
+    {
+        this.edad = edad;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
 }
