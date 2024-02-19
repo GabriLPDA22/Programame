caf220485f840cdcd92253d13cbe83fcd6ef814b
GabriLPDA22 <gsaiz.bajo@gmail.com>
2024-02-19 20:42:19 +0100

    añadido calse asiento con gets y sets

--- Challenge #2 - SALA_DE_CINE/main.java
+++ Challenge #2 - SALA_DE_CINE/main.java
@@ -1,4 +1,11 @@
 public class main {
 
-    
+    public static void main(String[] args) {
+        
+        //asiento miAsiento = new asiento(false, 'A', 'A'); 
+
+
+        System.out.println("ESta ocupado el asiento");
+
+    }
 }
