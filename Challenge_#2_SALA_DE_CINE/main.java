18beba787fccf344c74b8a79f90be3005801d81a
Gabriel Saiz <gsaiz.bajo@gmail.com>
2024-02-19 23:44:12 +0100

    Eliminado parte de abril en mi rama Fran

--- Challenge_#2_SALA_DE_CINE/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Espectador miEspectador = new Espectador("Adrian", 19, 100);
-
-        // Usando toString() para imprimir la información del espectador
-        System.out.println(miEspectador.toString()); // o simplemente System.out.println(miEspectador);
-    }
-}
