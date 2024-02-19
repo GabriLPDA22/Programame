d8f948a2fcb5c33b2b05232a86acced0daba4f1c
Adrian-Abril <a25620@svalero.com>
2024-02-19 20:06:08 +0100

    Comprobaciones clase Espectador

--- Challenge_#2_SALA_DE_CINE/main.java
+++ Challenge_#2_SALA_DE_CINE/main.java
@@ -1,4 +1,11 @@
 public class main
 {
-    
+    public static void main(String[] args) 
+    {
+        Espectador miEspectador = new Espectador("Adrian", 19, 100);    
+
+        System.out.println("Nombre " + miEspectador.getNombre());
+        System.out.println("Dinero " + miEspectador.getDinero());
+        System.out.println("Edad " + miEspectador.getEdad());
+    }
 }
