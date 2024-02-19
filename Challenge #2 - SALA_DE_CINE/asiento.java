b584674d1094400010eaec2f89d40b868abe852e
Adrian-Abril <a25620@svalero.com>
2024-02-19 20:19:13 +0100

    Añadios getters y setters

---
+++ Challenge #2 - SALA_DE_CINE/asiento.java
@@ -0,0 +1,15 @@
+public class asiento {
+    private boolean ocupado;
+
+    public asiento() {
+        this.ocupado = false;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+}
