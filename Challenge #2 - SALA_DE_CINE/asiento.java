babfc0d5097d7ad2c218947cff378793a20ca5b0
GabriLPDA22 <gsaiz.bajo@gmail.com>
2024-02-19 20:42:21 +0100

    Añadido clase asiento

--- Challenge #2 - SALA_DE_CINE/asiento.java
+++ Challenge #2 - SALA_DE_CINE/asiento.java
@@ -1,15 +1,34 @@
-public class asiento {
+public class Asiento {
+    private final char columna;
+    private final char fila;
     private boolean ocupado;
 
-    public asiento() {
+
+    // Constructor
+    public Asiento(char columna, char fila) {
+        this.columna = columna;
+        this.fila = fila;
         this.ocupado = false;
+    }
+
+    // Getters y métodos para manejar el asiento
+    public char getColumna() {
+        return columna;
+    }
+
+    public char getFila() {
+        return fila;
     }
 
     public boolean isOcupado() {
         return ocupado;
     }
 
-    public void setOcupado(boolean ocupado) {
-        this.ocupado = ocupado;
+    public void ocupar() {
+        this.ocupado = true;
+    }
+
+    public void desocupar() {
+        this.ocupado = false;
     }
 }
